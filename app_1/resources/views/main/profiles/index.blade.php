c63fa63e5bb7c8d7ec79d88a1e8075347290f1c3
Andrei Shantsev <44940699+Disooloo@users.noreply.github.com>
2021-11-08 19:17:42 +0500

    Update index.blade.php

--- app_1/resources/views/main/profiles/index.blade.php
+++ app_1/resources/views/main/profiles/index.blade.php
@@ -24,7 +24,7 @@
                         @if (Auth::user()->img == true)
                         <img src="{{ Auth::user()->img }}" alt="profile-foto">
                             @else
-                            <img  src="https://www.rcm.ac.uk/staffxml/images/missing.jpg" alt="profile-foto">
+                            <img  src="https://yt3.ggpht.com/a/AATXAJyJrARQbu2zRqfSEg1BIuh1pjerEEiLbhkgsOV_bA=s900-c-k-c0xffffffff-no-rj-mo" alt="profile-foto">
                         @endif
                     </div>
                 </div>
